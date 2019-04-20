ab2bac0f18149b7a1ab7ba68baedd5ae24f8f562
nnngrach <nnngrach@gmail.com>
2019-04-20 23:22:06 +0300

    Update routes.swift

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,13 +4,39 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "It works!!!!"
+        return "It works!!!!!"
     }
     
     // Basic "Hello, world!" example
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    
+    
+    // Get by name last
+    
+    // Get by name and timestamp
+    
+    // Get all to table view ?
+    
+    // Get all to backup ?
+    
+    
+    // Post by name
+    
+    // Post all to restore backup ?
+    
+    
+    // Delete by name last
+    
+    // Delete by name and timestamp
+    
+    // Delete all older that timestamp
+    
+    
+    
+    
 
     // Example of configuring a controller
     let todoController = TodoController()
